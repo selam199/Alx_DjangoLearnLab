2f3593524bd92854263bd1b08bf6e16904ec4b21
Selam Mulugeta <selammulugeta994@gmail.com>
2025-03-18 13:33:20 +0300

    Initial Setup and Project Configuration for a Django Blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -84,13 +84,17 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # Change from PostgreSQL to MySQL
+        'NAME':'peace',  # Replace with your MySQL database name
+        'USER': 'root',  # Replace with your MySQL database username
+        'PASSWORD': '1S2e3l4a5m6#',  # Replace with your MySQL database password
+        'HOST': '127.0.0.1',  # Or the host where your MySQL database is hosted
+        'PORT': '3306',  # The default MySQL port is 3306
     }
 }
+
 
 
 # Password validation
