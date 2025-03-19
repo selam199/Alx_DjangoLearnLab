a1bd5cebd63992b2f8bc781f5d6594e6984ffbd1
Selam Mulugeta <selammulugeta994@gmail.com>
2025-03-19 09:20:13 +0300

    Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -20,15 +20,16 @@ urlpatterns = [
     
     # Blog post URLs
     path("posts/", PostListView.as_view(), name="post_list"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post_edit"),
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
-    path("posts/<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
     path("posts/new/", PostCreateView.as_view(), name="post_new"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("posts/<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
+    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
+    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post_edit"),
     path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name='add_comment'),
     
     
-    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='add_comment'),
+    
+    
     path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='edit_comment'),
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete_comment'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
